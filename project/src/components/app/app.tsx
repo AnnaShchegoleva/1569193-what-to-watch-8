a133609611850a6dfffd8d76668ab1860549b766
AnnaShchegoleva <70628098+AnnaShchegoleva@users.noreply.github.com>
2021-10-13 16:47:51 -0700

    Приватный маршрут

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,11 +1,13 @@
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {AppRoute, AuthorizationStatus} from '../../const';
 import MainScreen from '../main-screen/main-screen';
 import SignIn from '../sign-in/sign-in';
 import MyList from '../my-list/my-list';
 import Film from '../film/film';
 import AddReview from '../add-review/add-review';
 import Player from '../player/player';
-
+import NotFound from '../not-found/not-found';
+import PrivateRoute from '../private-route/private-route';
 
 const filmPromo ={
   promoMovieTitle: 'The Grand Budapest Hotel',
@@ -17,23 +19,30 @@ function App(): JSX.Element {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path='/' exact>
+        <Route path={AppRoute.Main} exact>
           <MainScreen titleMovie={filmPromo.promoMovieTitle} genre={filmPromo.promoGenre} date={filmPromo.promoDate} />
         </Route>
-        <Route path='/sign-in' exact>
+        <Route path={AppRoute.SignIn} exact>
           <SignIn />
         </Route>
-        <Route path='/mylist' exact>
-          <MyList />
-        </Route>
-        <Route path='/films/:id' exact>
+        <PrivateRoute
+          exact
+          path={AppRoute.MyList}
+          render={() => <MyList />}
+          authorizationStatus={AuthorizationStatus.NoAuth}
+        >
+        </PrivateRoute>
+        <Route path={AppRoute.Film} exact>
           <Film />
         </Route>
-        <Route path='/films/:id/review' exact>
+        <Route path={AppRoute.AddReview} exact>
           <AddReview />
         </Route>
-        <Route path='/player/:id' exact>
+        <Route path={AppRoute.Player} exact>
           <Player />
+        </Route>
+        <Route>
+          <NotFound />
         </Route>
       </Switch>
     </BrowserRouter>
