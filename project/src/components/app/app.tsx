688e808f7736e7b4e4de9bba0f7944d9a07ce1ca
AnnaShchegoleva <70628098+AnnaShchegoleva@users.noreply.github.com>
2021-10-13 10:23:05 -0700

    Описание маршрутов с помощью компонентов

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,11 @@
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import MainScreen from '../main-screen/main-screen';
+import SignIn from '../sign-in/sign-in';
+import MyList from '../my-list/my-list';
+import Film from '../film/film';
+import AddReview from '../add-review/add-review';
+import Player from '../player/player';
+
 
 const filmPromo ={
   promoMovieTitle: 'The Grand Budapest Hotel',
@@ -8,7 +15,28 @@ const filmPromo ={
 
 function App(): JSX.Element {
   return (
-    < MainScreen titleMovie={filmPromo.promoMovieTitle} genre={filmPromo.promoGenre} date={filmPromo.promoDate} />
+    <BrowserRouter>
+      <Switch>
+        <Route path='/' exact>
+          <MainScreen titleMovie={filmPromo.promoMovieTitle} genre={filmPromo.promoGenre} date={filmPromo.promoDate} />
+        </Route>
+        <Route path='/sign-in' exact>
+          <SignIn />
+        </Route>
+        <Route path='/mylist' exact>
+          <MyList />
+        </Route>
+        <Route path='/films/:id' exact>
+          <Film />
+        </Route>
+        <Route path='/films/:id/review' exact>
+          <AddReview />
+        </Route>
+        <Route path='/player/:id' exact>
+          <Player />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
