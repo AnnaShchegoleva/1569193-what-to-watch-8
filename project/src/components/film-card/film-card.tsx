7f7b5e48d17c653af68e47deb4465b33cdb6bfa0
AnnaShchegoleva <70628098+AnnaShchegoleva@users.noreply.github.com>
2021-10-19 11:44:43 -0700

    Added state to film-card

--- project/src/components/film-card/film-card.tsx
+++ project/src/components/film-card/film-card.tsx
@@ -1,17 +1,21 @@
+import {useState} from 'react';
 import {FilmType} from '../../types/film';
+import {AppRoute} from '../../const';
+import {Link} from 'react-router-dom';
 
 type Props = {
   film: FilmType,
 }
 
 function FilmCard({film}:Props): JSX.Element {
+  const [, setState] = useState(0);
   return (
     <article className="small-film-card catalog__films-card">
-      <div className="small-film-card__image">
+      <div className="small-film-card__image" onMouseOver={() => {setState(film.id);}}>
         <img src={film.preview_image} alt={film.name} width="280" height="175"/>
       </div>
       <h3 className="small-film-card__title">
-        <a className="small-film-card__link" href={film.video_link}>{film.name}</a>
+        <Link className="small-film-card__link" to={AppRoute.Film.replace(':id', String(film.id))}>{film.name}</Link>
       </h3>
     </article>
   );
