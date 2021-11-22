88061850a4a7783be8d5e34a034f965c7b2ee17a
AnnaShchegoleva <70628098+AnnaShchegoleva@users.noreply.github.com>
2021-11-21 16:11:30 -0800

    fixed routing  in App

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -4,30 +4,24 @@ import {AppRoute/*, AuthorizationStatus*/} from '../../const';
 import MainScreen from '../main-screen/main-screen';
 
 import SignIn from '../sign-in/sign-in';
-/*
 import MyList from '../my-list/my-list';
-*/
 import Film from '../film/film';
 /*
 import AddReview from '../add-review/add-review';
 import Player from '../player/player';
 */
 import NotFound from '../not-found/not-found';
-/*
 import {isCheckAuth} from '../../utils';
 import PrivateRoute from '../private-route/private-route';
 import LoadingScreen from '../loading-screen/loading-screen';
-*/
 import browserHistory from '../../browser-history';
-/*
 import {useSelector} from 'react-redux';
 import {getAuthorizationStatus} from '../../store/user-process/selectors';
 import {getLoadedDataStatus} from '../../store/films-data/selectors';
-*/
 
 
 function App(): JSX.Element {
-  /*const authorizationStatus = useSelector(getAuthorizationStatus);
+  const authorizationStatus = useSelector(getAuthorizationStatus);
   const isDataLoaded = useSelector(getLoadedDataStatus);
 
   if (isCheckAuth(authorizationStatus) || !isDataLoaded) {
@@ -35,7 +29,6 @@ function App(): JSX.Element {
       <LoadingScreen />
     );
   }
-*/
   return (
     <BrowserRouter history={browserHistory}>
       <Switch>
@@ -45,12 +38,12 @@ function App(): JSX.Element {
         <Route path={AppRoute.SignIn} exact>
           <SignIn />
         </Route>
-        {/*<PrivateRoute
+        <PrivateRoute
           exact
           path={AppRoute.MyList}
           render={() => <MyList />}
         >
-        </PrivateRoute>*/}
+        </PrivateRoute>
         <Route exact path={AppRoute.Film}>
           <Film />
         </Route>
