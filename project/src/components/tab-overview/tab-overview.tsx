1039aa35b767817057a20331cae67790b6f50575
AnnaShchegoleva <70628098+AnnaShchegoleva@users.noreply.github.com>
2021-11-22 18:47:06 -0800

    Tabs fixed

--- project/src/components/tab-overview/tab-overview.tsx
+++ project/src/components/tab-overview/tab-overview.tsx
@@ -1,16 +1,42 @@
 import {FilmType} from '../../types/film';
+import {useMemo} from 'react';
 
 type TabOverviewProps = {
   film: FilmType;
 }
 
 function TabOverview({film}: TabOverviewProps): JSX.Element {
+
+  const rank = useMemo(() => {
+    if (film.rating < 3) {
+      return 'Bad';
+    }
+    if (film.rating < 5) {
+      return 'Normal';
+    }
+    if (film.rating < 8) {
+      return 'Good';
+    }
+    if (film.rating < 10) {
+      return 'Very good';
+    }
+    return 'Awesome';
+  }, [film.rating]);
+
+
+  const formatStarring = (starring: string[]): string => {
+    if (starring.length > 4) {
+      return `${starring.slice(0, 4).join(', ')} and other`;
+    }
+    return starring.join(', ');
+  };
+
   return (
     <>
       <div className="film-rating">
         <div className="film-rating__score">{film.rating}</div>
         <p className="film-rating__meta">
-          <span className="film-rating__level">Very good</span>
+          <span className="film-rating__level">{rank}</span>
           <span className="film-rating__count">{film.scoresCount} ratings</span>
         </p>
       </div>
@@ -19,7 +45,7 @@ function TabOverview({film}: TabOverviewProps): JSX.Element {
         <p>{film.description}</p>
 
         <p className="film-card__director"><strong>Director: {film.director}</strong></p>
-        <p className="film-card__starring"><strong>Starring: {film.starring}</strong></p>
+        <p className="film-card__starring"><strong>Starring: {formatStarring(film.starring)}</strong></p>
       </div>
 
     </>
